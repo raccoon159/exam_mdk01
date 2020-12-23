2d6a1b962f60af914b1bb5f5ca3e3c64bebd8b91
raccoon159 <67560006+raccoon159@users.noreply.github.com>
2020-12-23 11:18:09 +0500

    Add files via upload

---
+++ Экзамен01.01/Экзамен01.01/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Экзамен01._01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Услуга";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Колличество";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Сумма покупки";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Сумма принято";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Сдача";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(128, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 26);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(169, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(169, 168);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(169, 200);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(103, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(166, 27);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(27, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 34);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(160, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 339);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Экзамен01.01/Экзамен01.01/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace Экзамен01._01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Enabled = false;
+            textBox4.Enabled = false;
+            combo();
+            comboBox1.Text = "Выберите услугу";
+            
+        }
+        SqlConnection myConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+
+        double coun;
+        double sum;int i =0;
+        public void combo()//Вывод из бд в combobox
+        {
+            myConnection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Услуги", myConnection);
+            SqlDataReader reader;
+            try
+            {
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string name = reader.GetString(1);
+                    comboBox1.Items.Add(name);
+                }
+                myConnection.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }       
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox2.Text != "" && textBox3.Text != "")
+            {
+                double cum = Convert.ToDouble(textBox3.Text) - Convert.ToDouble(textBox1.Text) * coun;
+                if (cum < 0)
+                    MessageBox.Show("Возмите еще денег \n минимум еще "+ Math.Abs(cum)+" Рублей", "Ошибка");
+                else
+                    textBox4.Text = cum.ToString();
+
+            }
+
+            else MessageBox.Show("Заполните или выберите","Ошибка");
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {            
+            myConnection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT Цена FROM Услуги Where Код=@Код", myConnection);
+            command.Parameters.AddWithValue("@Код", comboBox1.SelectedIndex);
+
+            try
+            {
+                coun = Convert.ToDouble(command.ExecuteScalar().ToString());
+                textBox2.Text = coun.ToString();//Вывод цены
+                myConnection.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text != "")
+            {
+                string name = comboBox1.Text;
+                DateTime dt = DateTime.Now;
+
+                Properties.Settings.Default.fil++;//Чек
+                // Получаем массив байтов из нашего файла
+                byte[] textByteArray = File.ReadAllBytes("Test.docx");
+                // Массив данных
+                string[] data = new string[] {Properties.Settings.Default.fil.ToString(), dt.ToString(), Convert.ToString(name), Convert.ToString(textBox2.Text), Convert.ToString(textBox1.Text), Convert.ToString("="+ Convert.ToDouble(textBox1.Text) * coun), Convert.ToString(textBox3.Text + " Руб."), Convert.ToString(textBox4.Text)+"Руб." };
+                // Начинаем работу с потоком
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    // Записываем в поток наш word-файл
+                    stream.Write(textByteArray, 0, textByteArray.Length);
+                    // Открываем документ из потока с возможностью редактирования
+                    using (WordprocessingDocument doc = WordprocessingDocument.Open(stream, true))
+                    {
+                        // Ищем все закладки в документе
+                        var bookMarks = FindBookmarks(doc.MainDocumentPart.Document);
+
+                        int i = 0;
+                        foreach (var end in bookMarks)
+                        {
+                            // В документе встречаются какие-то служебные закладки
+                            // Таким способом отфильтровываем всё ненужное
+                            // end.Key содержит имена наших закладок
+                            if (end.Key != "i" && end.Key != "d" && end.Key != "date" && end.Key != "name" && end.Key != "summa" && end.Key != "kol" && end.Key != "sum" && end.Key != "ves" && end.Key != "ch" ) continue;
+                            // Создаём текстовый элемент
+                            var textElement = new Text(data[i].ToString());
+                            // Далее данный текст добавляем в закладку
+                            var runElement = new Run(textElement);
+                            end.Value.InsertAfterSelf(runElement);
+                            i++;
+                        }
+                    }
+                    // Записываем всё в наш файл 
+                    
+                    String path = (Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+                    String file = "чек "+ Properties.Settings.Default.fil+ ".docx";
+                    String filePath = Path.Combine(path, file);
+                    File.WriteAllBytes(filePath, stream.ToArray());
+                    MessageBox.Show(file + " находится на рабочем столе");
+                }
+                Properties.Settings.Default.Save();//сохраняем значения
+            }
+            else MessageBox.Show("Покупка","Ошибка");
+        }
+        private static Dictionary<string, BookmarkEnd> FindBookmarks(OpenXmlElement documentPart, Dictionary<string, BookmarkEnd> outs = null, Dictionary<string, string> bStartWithNoEnds = null)
+        {
+            if (outs == null) { outs = new Dictionary<string, BookmarkEnd>(); }
+            if (bStartWithNoEnds == null) { bStartWithNoEnds = new Dictionary<string, string>(); }
+
+            // Проходимся по всем элементам на странице Word-документа
+            foreach (var docElement in documentPart.Elements())
+            {
+                // BookmarkStart определяет начало закладки в рамках документа
+                // маркер начала связан с маркером конца закладки
+                if (docElement is BookmarkStart)
+                {
+                    var bookmarkStart = docElement as BookmarkStart;
+                    // Записываем id и имя закладки
+                    bStartWithNoEnds.Add(bookmarkStart.Id, bookmarkStart.Name);
+                }
+
+                // BookmarkEnd определяет конец закладки в рамках документа
+                if (docElement is BookmarkEnd)
+                {
+                    var bookmarkEnd = docElement as BookmarkEnd;
+                    foreach (var startName in bStartWithNoEnds)
+                    {
+                        // startName.Key как раз и содержит id закладки
+                        // здесь проверяем, что есть связь между началом и концом закладки
+                        if (bookmarkEnd.Id == startName.Key)
+                            // В конечный массив добавляем то, что нам и нужно получить
+                            outs.Add(startName.Value, bookmarkEnd);
+                    }
+                }
+                // Рекурсивно вызываем данный метод, чтобы пройтись по всем элементам
+                // word-документа
+                FindBookmarks(docElement, outs, bStartWithNoEnds);
+            }
+            return outs;
+        }
+    }
+}
